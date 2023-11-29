13b6705485d4836f5f1610551265eaa49d6c1eb1
Marco Antonio Quisberth Barrera <151577625+MarcoAntonioQB@users.noreply.github.com>
2023-11-29 02:17:18 +0000

    Añadiendo las opciones del usauario y del sistema

---
+++ game.py
@@ -0,0 +1,7 @@
+import random
+
+accion_usuario = input("Seleccione una opción entre (piedra, papel o tijeras)")
+acciones = ["piedra", "papel", "tijeras"]
+accion_sistema = random.choice(acciones)
+
+print(f"\n Elegiste {accion_usuario}, el juego eligió {accion_sistema}.\n")
